12973f697a54821a4cddd46c4bebedd6b2522ba1
Stanley Stuart <stanley@stan.li>
2013-12-28 22:03:44 -0600

    help! i need somebody! help! get some more tests passing! help!

--- test/extractor_test.js
+++ test/extractor_test.js
@@ -60,10 +60,30 @@ describe("I18nExtractor", function(){
     });
   });
 
+  describe('t calls inside block helpers', function(){
+
+    it('still extracts keys', function(){
+      assert.deepEqual(extract('{{#each foo}}{{#t "foo"}}Foo{{/t}}{{/each}}'),
+                       {foo: 'Foo'});
+    });
+  });
+
   describe('wrappers', function(){
 
     it('infers wrappers', function(){
-      throw new Error('TODO!');
+      var value = extract('{{#t "foo"}}Be sure to <a href="{{url}}">log in</a>. <b>Don\'t</b> you <b>dare</b> forget!!!{{/t}}');
+
+      assert.deepEqual(value, {foo: "Be sure to *log in*. **Don't** you **dare** forget!!!"});
+    });
+
+    it('allows empty tags on either side of the wrapper', function(){
+      var result = extract('{{#t "bar"}}you can <button><i class="icon-email"></i>send an email</button>{{/t}}');
+
+      assert.deepEqual(result, {bar: 'you can *send an email*'});
+
+      result = extract('{{#t "baz"}}this is <b>so cool!<img /></b>{{/t}}');
+
+      assert.deepEqual(result, { baz: 'this is *so cool!*'});
     });
   });
 });
